ef18896605646adb80e568dfe1716410e0ba6826
atriexz4pato <kungapatrick81@gmail.com>
2024-08-23 08:42:25 +0300

    creating events migration

---
+++ database/migrations/2024_08_23_053802_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('description')->nullable(false);
+            $table->time('start_time')->nullable(false);
+            $table->time('end_time')->nullable(false);
+            $table->string('venue')->nullable(false);
+            $table->date('date')->nullable(false);
+            $table->string('image_url')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
