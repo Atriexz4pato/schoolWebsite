c838be0ca62dd39e3a946cb0319159583cddbc22
atriexz4pato <kungapatrick81@gmail.com>
2024-08-23 08:42:06 +0300

    introduce event model

---
+++ app/Models/Event.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+    protected $fillable =[
+        'date',
+        'description',
+        'venue',
+        'title',
+        'image_url',
+        'start_time',
+        'end_time',
+    ];
+}
